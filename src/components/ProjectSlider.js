ef7e2de6ba18003e664a3946f999703608578504
Pfenei <adrsanrod2@gmail.com>
2023-08-19 08:35:39 +0100

    started projects work

---
+++ src/components/ProjectSlider.js
@@ -0,0 +1,11 @@
+import "./projects.json";
+const ProjectSlider =() =>{
+
+    return(
+        <div className="projects">
+
+        </div>
+    );
+}
+
+export default ProjectSlider;
---
+++ src/components/Projects.js
@@ -0,0 +1,8 @@
+import ProjectSlider from "./ProjectSlider";
+import "./ProjectStyle.css";
+const Projects = () =>{
+    return(
+        <ProjectSlider/>
+    );
+}
+export default Projects
