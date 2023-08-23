dfde3bc7e66c76cea1c03b2c397e28d8e4ecc127
Pfenei <adrsanrod2@gmail.com>
2023-08-23 21:19:54 +0100

    project ccards wip

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,12 @@
-
-const ProjectCard = ({project}) =>{
-    return(
-        <div className="card" key={project.name}>
-            <img src={require(project.img)}/>
-            <h2 className="card-title">{project.name}</h2>
-            <p className="card-body">{project.description}</p>
-            <a href={project.url} className="button" ></a>
+const ProjectCard = ({project}) => {
+    return (
+        <div className="card" style={{backgroundImage: 'url("' + project.img + '")', backgroundSize: "cover"}}
+             key={project.name}>
+            <div className="card-content">
+                <h2 className="card-title">{project.name}</h2>
+                <p className="card-body">{project.description}</p>
+                <a href={project.url} className="button"></a>
+            </div>
         </div>
     );
 }
